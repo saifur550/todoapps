6f1b6f1b27bbdbec30c05f475c0dee1225aa7d6f
saifur550 <srstap550@gmail.com>
2022-02-25 21:54:12 +0600

    todo apps

--- js/apps.js
+++ js/apps.js
@@ -24,7 +24,7 @@ addBtn.addEventListener('click',function (e){
 
 
     //call function
-   addNewItem(taskList);
+   addNewItem(inputValue);
    
   
 });
@@ -32,8 +32,14 @@ addBtn.addEventListener('click',function (e){
 
 function addNewItem (text){
     const item = document.createElement('div');
-    item.classList = 'item'
+    item.className= 'item';
     item.innerHTML =
+    `
+    <li>${text}</li>
+    <button class="edit ">Edit <i class="fas fa-pen eventOff "></i></button>
+    <button class="completed  <i class="fas fa-check eventOff "></i></button>
+    <button class="delete ">Delete <i class="fas fa-trash-can eventOff "></i></button>
+    `
     
 
   
@@ -45,3 +51,64 @@ function addNewItem (text){
 
 
 // get the input value 
+
+taskList.addEventListener('click', function(event) {
+    
+    if(event.target.className == "delete"){
+        // console.log('click edit delete');
+        deleteItem(event);
+    }
+    else if(event.target.className == "completed"){
+        // console.log('click edit completed');
+        completeTask(event);
+    }
+    else if(event.target.className == "edit"){
+        // console.log('click edit edit');
+        editItem(event)
+    }
+}) 
+
+
+
+function deleteItem(event){
+    event.target.parentElement.remove();
+}
+
+
+
+
+function completeTask(event){
+    const li = event.target.parentElement.firstElementChild;
+    li.classList.toggle('colorBlur')
+    
+}
+
+
+function editItem(event){
+    const li = event.target.parentElement.firstElementChild;
+    const preText = li.innerText;
+    li.innerHTML = ''
+  
+
+    //create input 
+    const createInput = document.createElement('input');
+    createInput.type ='text';
+    createInput.value = preText;
+    //add even 
+
+    createInput.addEventListener('keypress', function(e){
+        if(e.key == "Enter"){
+            const modifiedName = e.target.value ;
+            li.innerHTML = '';
+            li.innerText = modifiedName;
+            // console.log(modifiedName);
+            event.target.style.display ='inline';
+        }
+    })
+
+    li.appendChild(createInput);
+    event.target.style.display ='none'
+
+
+
+}
